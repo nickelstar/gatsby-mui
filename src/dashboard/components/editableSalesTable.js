cb1b227dfb86a41d7a247b00238d110d9e28d79f
Nick Gates <nickcgates@gmail.com>
2020-02-21 00:10:52 +0000

    Added and editable table powered by GraphQL.

--- src/dashboard/components/editableSalesTable.js
+++ src/dashboard/components/editableSalesTable.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { forwardRef } from 'react';
 import MaterialTable from 'material-table';
+import API, { graphqlOperation } from '@aws-amplify/api';
+
+import { listCountryFoodSaless } from '../../graphql/queries';
+import {
+    onCreateCountryFoodSales,
+    onUpdateCountryFoodSales,
+    onDeleteCountryFoodSales
+} from '../../graphql/subscriptions';
+import {
+    createCountryFoodSales,
+    updateCountryFoodSales,
+    deleteCountryFoodSales
+} from '../../graphql/mutations';
 
 import AddBox from '@material-ui/icons/AddBox';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
@@ -13,10 +26,10 @@ import Edit from '@material-ui/icons/Edit';
 import FilterList from '@material-ui/icons/FilterList';
 import FirstPage from '@material-ui/icons/FirstPage';
 import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
+// import Remove from '@material-ui/icons/Remove';
+// import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+// import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const tableIcons = {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
@@ -33,75 +46,170 @@ const tableIcons = {
     PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
     ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
     Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    // SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
     // ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
     // ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
 }
 
+// Action Types
+const QUERY = 'QUERY';
+const CREATE = 'CREATE';
+const UPDATE = 'UPDATE';
+const DELETE = 'DELETE';
+
+const CREATE_SUBSCRIPTION = 'CREATE_SUBSCRIPTION';
+const UPDATE_SUBSCRIPTION = 'UPDATE_SUBSCRIPTION';
+const DELETE_SUBSCRIPTION = 'DELETE_SUBSCRIPTION';
+
+const initialState = {
+    countryFoodSaless: [],
+};
+
+const updateObjectInArray = (array, replacementObject) => {
+    return array.map((item, index) => {
+        if (item.id !== replacementObject.id) {
+            // This isn't the item we care about - keep it as-is
+            return item
+        }
+        // Otherwise, this is the one we want - return an updated value
+        return {
+            ...item,
+            ...replacementObject
+        }
+    })
+}
+
+const deleteObjectInArray = (array, replacementObject) => {
+    return array.filter((item) => {
+        if (item.id !== replacementObject.id) {
+            // This isn't the item we care about - keep it as-is
+            return item
+        }
+    })
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case QUERY:
+            return { ...state, countryFoodSaless: action.countryFoodSaless };
+        case CREATE:
+        case CREATE_SUBSCRIPTION:
+            return { ...state, countryFoodSaless: [...state.countryFoodSaless, action.countryFoodSales] }
+        case UPDATE:
+        case UPDATE_SUBSCRIPTION:
+            return {
+                ...state, countryFoodSaless: updateObjectInArray(state.countryFoodSaless, action.countryFoodSales)
+            }
+        case DELETE:
+        case DELETE_SUBSCRIPTION:
+            return {
+                ...state, countryFoodSaless: deleteObjectInArray(state.countryFoodSaless, action.countryFoodSales)
+            }
+        default:
+            return state;
+    }
+};
+
 export default function EditableSalesTable() {
-    const [state, setState] = React.useState({
-        columns: [
-            { title: 'Name', field: 'name' },
-            { title: 'Surname', field: 'surname' },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-            {
-                title: 'Birth Place',
-                field: 'birthCity',
-                lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-            },
-        ],
-        data: [
-            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-            {
-                name: 'Zerya Betül',
-                surname: 'Baran',
-                birthYear: 2017,
-                birthCity: 34,
-            },
-        ],
-    });
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        async function getData() {
+            const salesData = await API.graphql(graphqlOperation(listCountryFoodSaless));
+            dispatch({ type: QUERY, countryFoodSaless: salesData.data.listCountryFoodSaless.items });
+        }
+        getData();
+
+        const onCreateSubscription = API.graphql(graphqlOperation(onCreateCountryFoodSales)).subscribe({
+            next: (eventData) => {
+                const countryFoodSales = eventData.value.data.onCreateCountryFoodSales;
+                dispatch({ type: CREATE_SUBSCRIPTION, countryFoodSales });
+            }
+        });
+
+        const onUpdateSubscription = API.graphql(graphqlOperation(onUpdateCountryFoodSales)).subscribe({
+            next: (eventData) => {
+                const countryFoodSales = eventData.value.data.onUpdateCountryFoodSales;
+                dispatch({ type: UPDATE_SUBSCRIPTION, countryFoodSales });
+            }
+        });
+
+        const onDeleteSubscription = API.graphql(graphqlOperation(onDeleteCountryFoodSales)).subscribe({
+            next: (eventData) => {
+                const countryFoodSales = eventData.value.data.onDeleteCountryFoodSales;
+                dispatch({ type: DELETE_SUBSCRIPTION, countryFoodSales });
+            }
+        });
+
+        return () => {
+            onCreateSubscription.unsubscribe();
+            onUpdateSubscription.unsubscribe();
+            onDeleteSubscription.unsubscribe();
+        }
+    }, []);
+
+    const tableColumns = [
+        { title: 'Country Code', field: 'countryCode' },
+        { title: 'Donut Sales', field: 'donutSales', type: 'numeric' },
+        { title: 'Kebab Sales', field: 'kebabSales', type: 'numeric' },
+        { title: 'Sandwich Sales', field: 'sandwichSales', type: 'numeric' },
+        { title: 'Burger Sales', field: 'burgerSales', type: 'numeric' },
+        { title: 'Hotdog Sales', field: 'hotdogSales', type: 'numeric' },
+    ]
 
     return (
         <MaterialTable
             title="Editable Example"
             icons={tableIcons}
-            columns={state.columns}
-            data={state.data}
+            columns={tableColumns}
+            data={state.countryFoodSaless}
             editable={{
-                onRowAdd: newData =>
-                    new Promise(resolve => {
+                onRowAdd: (newData) => {
+                    return new Promise(async (resolve) => {
+                        resolve();
+
                         setTimeout(() => {
-                            resolve();
-                            setState(prevState => {
-                                const data = [...prevState.data];
-                                data.push(newData);
-                                return { ...prevState, data };
-                            });
-                        }, 600);
-                    }),
-                onRowUpdate: (newData, oldData) =>
-                    new Promise(resolve => {
-                        setTimeout(() => {
-                            resolve();
-                            if (oldData) {
-                                setState(prevState => {
-                                    const data = [...prevState.data];
-                                    data[data.indexOf(oldData)] = newData;
-                                    return { ...prevState, data };
-                                });
+                            dispatch({ type: CREATE, countryFoodSales: newData });
+                        }, 0);
+
+                        await API.graphql(graphqlOperation(createCountryFoodSales, {
+                            input: {
+                                ...newData
                             }
-                        }, 600);
-                    }),
-                onRowDelete: oldData =>
-                    new Promise(resolve => {
+                        }))
+                    })
+                },
+                onRowUpdate: (newData, oldData) => {
+                    return new Promise(async (resolve) => {
+                        resolve();
+
                         setTimeout(() => {
-                            resolve();
-                            setState(prevState => {
-                                const data = [...prevState.data];
-                                data.splice(data.indexOf(oldData), 1);
-                                return { ...prevState, data };
-                            });
-                        }, 600);
+                            dispatch({ type: UPDATE, countryFoodSales: newData });
+                        }, 0);
+
+
+                        await API.graphql(graphqlOperation(updateCountryFoodSales, {
+                            input: {
+                                ...newData
+                            }
+                        }))
+                    })
+                },
+
+                onRowDelete: oldData =>
+                    new Promise(async (resolve) => {
+                        resolve();
+
+                        setTimeout(() => {
+                            dispatch({ type: DELETE, countryFoodSales: oldData });
+                        }, 0);
+
+                        await API.graphql(graphqlOperation(deleteCountryFoodSales, {
+                            input: {
+                                id: oldData.id
+                            }
+                        }))
                     }),
             }}
         />
